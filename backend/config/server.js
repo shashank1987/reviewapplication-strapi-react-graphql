994cdc3aa0103fcf577080dbb3fe128244d33103
Shashikant Naik <shashikant.naik87@gmail.com>
2021-08-15 17:48:29 +0530

    React with Strapi and GraphQl

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd86f46575b105ded70abed6c65eb2c62'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b4a5386-85a0-42d8-84ce-c892f015cb40'
+};
